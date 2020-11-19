caad653020d3b8da1d7911ac7217fd2d28cb41a8
Aditya Gupta <adi.h4ck3r@gmail.com>
2020-11-19 23:31:28 +0530

    Create Triangle_quest_2.py

---
+++ Python3/Triangle_quest_2.py
@@ -0,0 +1,19 @@
+for i in range(1,int(input())+1): #More than 2 lines will result in 0 score. Do not leave a blank line also
+    print(((10**i - 1)//9)**2)
+
+
+
+'''
+Sample Input
+
+5
+
+Sample Output
+
+1
+121
+12321
+1234321
+123454321
+
+'''
