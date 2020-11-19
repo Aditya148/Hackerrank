5de17d00424880184787507c7f1da3044326830a
Aditya Gupta <adi.h4ck3r@gmail.com>
2020-11-19 23:51:56 +0530

    Create Xml1_find_the_score.py

---
+++ Python3/Xml1_find_the_score.py
@@ -0,0 +1,31 @@
+import sys
+import xml.etree.ElementTree as etree
+
+def get_attr_number(node):
+    return etree.tostring(node).count(b'=')
+
+if __name__ == '__main__':
+    sys.stdin.readline()
+    xml = sys.stdin.read()
+    tree = etree.ElementTree(etree.fromstring(xml))
+    root = tree.getroot()
+    print(get_attr_number(root))
+    
+    
+    
+'''
+Sample Input
+
+6
+<feed xml:lang='en'>
+    <title>HackerRank</title>
+    <subtitle lang='en'>Programming challenges</subtitle>
+    <link rel='alternate' type='text/html' href='http://hackerrank.com/'/>
+    <updated>2013-12-25T12:00:00</updated>
+</feed>
+
+Sample Output
+
+5
+
+'''
