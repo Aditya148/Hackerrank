05c6ca8773d9b819b83b82180a3cf85bdf68ec35
Aditya Gupta <adi.h4ck3r@gmail.com>
2020-11-20 22:05:30 +0530

    Create Introduction_to_sets.py

---
+++ Python3/Introduction_to_sets.py
@@ -0,0 +1,21 @@
+def average(array):
+    return sum(set(array))/len(set(array))
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().split()))
+    result = average(arr)
+    print(result)
+    
+    
+'''
+Sample Input
+
+10
+161 182 161 154 176 170 167 171 170 174
+
+Sample Output
+
+169.375
+
+'''
