0d17e366ff4127a80712e57b9bac42c2f13380e0
Aditya Gupta <adi.h4ck3r@gmail.com>
2020-11-20 13:12:10 +0530

    Create Sparse_arrays.py

---
+++ Data_Structures/Sparse_arrays.py
@@ -0,0 +1,61 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the matchingStrings function below.
+def matchingStrings(strings, queries):
+    ans = []
+    for q in queries:
+        ans.append(strings.count(q))
+    return ans
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    strings_count = int(input())
+
+    strings = []
+
+    for _ in range(strings_count):
+        strings_item = input()
+        strings.append(strings_item)
+
+    queries_count = int(input())
+
+    queries = []
+
+    for _ in range(queries_count):
+        queries_item = input()
+        queries.append(queries_item)
+
+    res = matchingStrings(strings, queries)
+
+    fptr.write('\n'.join(map(str, res)))
+    fptr.write('\n')
+
+    fptr.close()
+
+
+
+
+'''
+Sample Input 1
+4
+aba
+baba
+aba
+xzxb
+3
+aba
+xzxb
+ab
+
+Sample Output 1
+
+2
+1
+0
+'''
