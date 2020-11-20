e1c6d9275d039d73ae1a4c61e790f285804da70c
Aditya Gupta <adi.h4ck3r@gmail.com>
2020-11-20 23:22:20 +0530

    Create Set_add.py

---
+++ Python3/Set_add.py
@@ -0,0 +1,24 @@
+n = int(input())
+a = set()
+for i in range(n):
+    a.add(input())
+print(len(a))
+
+
+'''
+Sample Input
+
+7
+UK
+China
+USA
+France
+New Zealand
+UK
+France 
+
+Sample Output
+
+5
+
+'''
