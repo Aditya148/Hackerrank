8300eee1c2feeddfc3e32d9b86fb7d29e97d67e5
Aditya Gupta <adi.h4ck3r@gmail.com>
2020-11-20 23:42:23 +0530

    Create Set_intersection.py

---
+++ Python3/Set_intersection.py
@@ -0,0 +1,20 @@
+m = int(input())
+a = set(map(int, input().split()))
+n = int(input())
+b = set(map(int, input().split()))
+print(len(a.intersection(b)))
+
+
+'''
+Sample Input
+
+9
+1 2 3 4 5 6 7 8 9
+9
+10 1 2 3 11 21 55 6 8
+
+Sample Output
+
+5
+
+'''
