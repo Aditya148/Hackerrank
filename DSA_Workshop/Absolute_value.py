ae314706842427abbe3db032e8c39af48110ab55
Aditya Gupta <adi.h4ck3r@gmail.com>
2020-11-21 20:48:06 +0530

    Create Absolute_value.py

---
+++ DSA_Workshop/Absolute_value.py
@@ -0,0 +1,13 @@
+def absolute(I):
+    return abs(I)
+    
+def main():
+    T = int(input()) #Input the number of testcases
+    while(T > 0):
+        I = int(input()) #input number
+        print(absolute(I)) #Call function and print
+        T -= 1 #Reduce number of testcases
+
+
+if __name__ == "__main__":
+    main()
