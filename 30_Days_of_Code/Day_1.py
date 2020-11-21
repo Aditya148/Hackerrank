8768874b381280991cc330697a1387f983ff47c5
Aditya Gupta <adi.h4ck3r@gmail.com>
2020-11-21 12:04:22 +0530

    Create Day_1.py

---
+++ 30_Days_of_Code/Day_1.py
@@ -0,0 +1,22 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+print(i+int(input()))
+print(d+float(input()))
+print(s+input())
+
+
+'''
+Sample Input
+
+12
+4.0
+is the best place to learn and practice coding!
+
+Sample Output
+
+16
+8.0
+HackerRank is the best place to learn and practice coding!
+
+'''
