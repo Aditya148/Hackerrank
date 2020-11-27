06e7f91bf06cb4ec19b23b8d9e49c7f1787279a0
Aditya Gupta <adi.h4ck3r@gmail.com>
2020-11-27 11:36:21 +0530

    Create Day_7.py

---
+++ 30_Days_of_Code/Day_7.py
@@ -0,0 +1,28 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+    print(*arr[-1::-1])
+    
+    
+'''
+Sample Input
+
+4
+1 4 3 2
+
+Sample Output
+
+2 3 4 1
+
+'''
