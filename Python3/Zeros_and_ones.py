5899ce1edc1fd7c7adac659541d15392d9fca843
Aditya Gupta <adi.h4ck3r@gmail.com>
2020-12-05 17:18:28 +0530

    Create Zeros_and_ones.py

---
+++ Python3/Zeros_and_ones.py
@@ -0,0 +1,40 @@
+import numpy
+
+n = tuple(map(int, input().split()))
+print(numpy.zeros(n, dtype = numpy.int))
+print(numpy.ones(n, dtype = numpy.int))
+
+
+
+'''
+Sample Input 0
+
+3 3 3
+
+Sample Output 0
+
+[[[0 0 0]
+  [0 0 0]
+  [0 0 0]]
+
+ [[0 0 0]
+  [0 0 0]
+  [0 0 0]]
+
+ [[0 0 0]
+  [0 0 0]
+  [0 0 0]]]
+[[[1 1 1]
+  [1 1 1]
+  [1 1 1]]
+
+ [[1 1 1]
+  [1 1 1]
+  [1 1 1]]
+
+ [[1 1 1]
+  [1 1 1]
+  [1 1 1]]]
+
+
+'''
