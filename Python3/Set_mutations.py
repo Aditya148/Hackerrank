4d97be29caaabc0fae04c696bb2aa584480e676e
Aditya Gupta <adi.h4ck3r@gmail.com>
2020-12-05 16:02:41 +0530

    Create Set_mutations.py

---
+++ Python3/Set_mutations.py
@@ -0,0 +1,32 @@
+n = int(input())
+s = set([int(i) for i in input().split()])
+t = int(input())
+for i in range(t):
+    cmd, l = [j for j in input().split()]
+    arr = set([int(j) for j in input().split()])
+    getattr(s, cmd)(arr)
+print(sum(s))
+
+
+
+
+'''
+Sample Input
+
+ 16
+ 1 2 3 4 5 6 7 8 9 10 11 12 13 14 24 52
+ 4
+ intersection_update 10
+ 2 3 5 6 8 9 1 4 7 11
+ update 2
+ 55 66
+ symmetric_difference_update 5
+ 22 7 35 62 58
+ difference_update 7
+ 11 22 35 55 58 62 66
+
+Sample Output
+
+38
+
+'''
