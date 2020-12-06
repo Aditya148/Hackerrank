ff3694ecdac560e4bf386466fd7ac889bb1adf40
Aditya Gupta <adi.h4ck3r@gmail.com>
2020-12-06 20:33:04 +0530

    Create Iterables_and_iterators.py

---
+++ Python3/Iterables_and_iterators.py
@@ -0,0 +1,25 @@
+from itertools import combinations
+
+N = int(input())
+L = input().split()
+K = int(input())
+
+C = list(combinations(L, K))
+F = filter(lambda c: 'a' in c, C)
+print("{0:.3}".format(len(list(F))/len(C)))
+
+
+
+
+'''
+Sample Input
+
+4 
+a a c d
+2
+
+Sample Output
+
+0.8333
+
+'''
