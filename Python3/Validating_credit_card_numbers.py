ed6240a0d86714c7077e06c0816fa77e7207537b
Aditya Gupta <adi.h4ck3r@gmail.com>
2020-12-06 17:46:14 +0530

    Create Validating_credit_card_numbers.py

---
+++ Python3/Validating_credit_card_numbers.py
@@ -0,0 +1,35 @@
+import re
+val = re.compile(
+    r"^"
+    r"(?!.*(\d)(-?\1){3})"
+    r"[456]"
+    r"\d{3}"
+    r"(?:-?\d{4}){3}"
+    r"$")
+for _ in range(int(input().strip())):
+    print("Valid" if val.search(input().strip()) else "Invalid")
+    
+    
+    
+    
+'''
+Sample Input
+
+6
+4123456789123456
+5123-4567-8912-3456
+61234-567-8912-3456
+4123356789123456
+5133-3367-8912-3456
+5123 - 3567 - 8912 - 3456
+
+Sample Output
+
+Valid
+Valid
+Invalid
+Valid
+Invalid
+Invalid
+
+'''
