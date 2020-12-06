6148810832d914c6cfef177c695d78a8b02ea041
Aditya Gupta <adi.h4ck3r@gmail.com>
2020-12-06 20:01:34 +0530

    Create Group_groups_groupdict.py

---
+++ Python3/Group_groups_groupdict.py
@@ -0,0 +1,15 @@
+import re
+m = re.search(r'([a-zA-Z0-9])\1+', input().strip())
+print(m.group(1) if m else -1)
+
+
+'''
+Sample Input
+
+..12345678910111213141516171820212223
+
+Sample Output
+
+1
+
+'''
