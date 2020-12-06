3258e157890530f8111d805b4fb5cce14a2dace3
Aditya Gupta <adi.h4ck3r@gmail.com>
2020-12-06 12:24:09 +0530

    Create Time_delta.py

---
+++ Python3/Time_delta.py
@@ -0,0 +1,27 @@
+from datetime import datetime as dt
+
+fmt = '%a %d %b %Y %H:%M:%S %z'
+for i in range(int(input())):
+    print(int(abs((dt.strptime(input(), fmt) - 
+                   dt.strptime(input(), fmt)).total_seconds())))
+
+
+
+
+
+'''
+Sample Input 0
+
+2
+Sun 10 May 2015 13:54:36 -0700
+Sun 10 May 2015 13:54:36 -0000
+Sat 02 May 2015 19:54:36 +0530
+Fri 01 May 2015 13:54:36 -0000
+
+Sample Output 0
+
+25200
+88200
+
+
+'''
