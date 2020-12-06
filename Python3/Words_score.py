9479e66aecdce47bd8adfe2437131a37e617c64a
Aditya Gupta <adi.h4ck3r@gmail.com>
2020-12-06 20:42:27 +0530

    Update Words_score.py

--- Python3/Words_score.py
+++ Python3/Words_score.py
@@ -2,23 +2,12 @@ def is_vowel(letter):
     return letter in ['a', 'e', 'i', 'o', 'u', 'y']
 
 def score_words(words):
-    score = 0
-    for word in words:
-        num_vowels = 0
-        for letter in word:
-            if letter in 'aeiou':
-                num_vowels += 1
-        if num_vowels % 2 == 0:
-            score += 2
-        else:
-            score += 1
-    return score
+    return sum(sum(char in 'aeiouy' for char in word) % 2 or 2 for word in words)
 
 
 n = int(input())
 words = input().split()
 print(score_words(words))
-
 
 
 '''
