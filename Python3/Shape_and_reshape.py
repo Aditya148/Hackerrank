4c7aa46b11f9aabb6ace06bfa1539a5a25eb41cf
Aditya Gupta <adi.h4ck3r@gmail.com>
2020-12-06 20:12:51 +0530

    Create Shape_and_reshape.py

---
+++ Python3/Shape_and_reshape.py
@@ -0,0 +1,18 @@
+import numpy as np
+print(np.reshape(np.array([int(i) for i in input().split()]), (3,3)))
+
+
+
+
+'''
+Sample Input
+
+1 2 3 4 5 6 7 8 9
+
+Sample Output
+
+[[1 2 3]
+ [4 5 6]
+ [7 8 9]]
+
+'''
